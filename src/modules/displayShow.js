1cd7a3b0b2bab6f4574f8f5840576f67ef1fc44b
levy002 <levy.ukwi002@gmail.com>
2022-03-09 11:19:29 +0200

    Display shows to homepage

--- src/modules/displayShow.js
+++ src/modules/displayShow.js
@@ -1,19 +1,27 @@
+import getshow from './getShows.js';
+
 const allShows = document.querySelector('.allShows');
 
-const displayShows = (show) => {
-  const showInfo = document.createElement('li');
-  showInfo.id = show.id;
-  showInfo.className = 'show';
-  showInfo.innerHTML = `
-  <img id="show-img" src=${show.image} alt="show Image">
+const displayShows = async () => {
+  const fetchedShows = await getshow();
+  const Shows = fetchedShows.slice(0, 20);
+
+  Shows.map((show) => {
+    const showInfo = document.createElement('li');
+    showInfo.id = show.id;
+    showInfo.className = 'show';
+    showInfo.innerHTML = `
+  <img id="show-img" src=${show.image.medium} alt="show Image">
   <div id="like-show">
     <h2 id="show-title">${show.name}</h2>
     <i class="fa fa-heart-o" id="like-icon" aria-hidden="true"></i>
   </div>
-  <p id="likes">${show.likes} likes</p>
+  <p id="likes">0 likes</p>
   <button type="button" id="comment-btn">Comments</button>
   `;
-  allShows.appendChild(showInfo);
+    allShows.appendChild(showInfo);
+    return show;
+  });
 };
 
 export default displayShows;
