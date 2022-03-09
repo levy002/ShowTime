fff998c4c071cff22e6c3a21167bc903b1b3a062
levy002 <levy.ukwi002@gmail.com>
2022-03-09 11:18:55 +0200

    Fetching shows to API

--- src/modules/getShows.js
+++ src/modules/getShows.js
@@ -1,24 +1,8 @@
-import displayShows from './displayShow.js';
-import { getLikesData } from './likesCounter.js';
+const url = 'https://api.tvmaze.com/shows';
 
-const shows = [];
-const showId = [
-  200, 201, 78, 87, 67, 300, 123, 878, 96, 3458, 130, 654, 1, 789, 35, 5679, 456, 7890, 44, 100];
-const url = 'https://api.tvmaze.com/shows/';
-
-const getshow = showId.map(async (id) => {
-  let data = await fetch(`${url}${id}`);
-  data = await data.json();
-  const show = {
-    name: data.name,
-    image: data.image.original,
-    id: data.id,
-    likes: 0,
-  };
-
-  shows.push(show);
-  displayShows(show);
-  return data;
-});
+const getshow = async () => {
+  const data = await fetch(`${url}`);
+  return data.json();
+};
 
 export default getshow;
