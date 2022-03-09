12f67cf121714268889f7b23ad813470f5d3e9f3
levy002 <levy.ukwi002@gmail.com>
2022-03-09 16:15:38 +0200

    Display likes of each show on home page

--- src/modules/displayShow.js
+++ src/modules/displayShow.js
@@ -1,10 +1,24 @@
 import getshow from './getShows.js';
+import { getLikesData } from './likesCounter.js';
 
 const allShows = document.querySelector('.allShows');
 
 const displayShows = async () => {
   const fetchedShows = await getshow();
   const Shows = fetchedShows.slice(0, 20);
+
+  const likesArray = await getLikesData();
+  likesArray.map((a) => {
+    Shows.map((show) => {
+      if (a.item_id === show.id) {
+        show.likes = a.likes;
+      } else if (show.likes === undefined) {
+        show.likes = 0;
+      }
+      return show;
+    });
+    return a;
+  });
 
   Shows.map((show) => {
     const showInfo = document.createElement('li');
@@ -16,7 +30,7 @@ const displayShows = async () => {
     <h2 id="show-title">${show.name}</h2>
     <i class="fa fa-heart-o" id="like-icon" aria-hidden="true"></i>
   </div>
-  <p id="likes">0 likes</p>
+  <p id="likes">${show.likes} likes</p>
   <button type="button" id="comment-btn">Comments</button>
   `;
     allShows.appendChild(showInfo);
