b4a0a99b7f92e1b8bc2b79ac361c30bb17bc02c2
levy002 <levy.ukwi002@gmail.com>
2022-03-09 10:06:01 +0200

    Refactor displayShow function

--- src/modules/displayShow.js
+++ src/modules/displayShow.js
@@ -1,48 +1,19 @@
 const allShows = document.querySelector('.allShows');
 
-const displayShows = (data) => {
-  allShows.replaceChildren();
-  data.map((show) => {
-    const showInfo = document.createElement('li');
-    showInfo.id = show.id;
-    showInfo.className = 'show';
-
-    const showImg = document.createElement('img');
-    showImg.id = 'show-img';
-    showImg.src = show.image;
-    showImg.alt = 'show Image';
-    showInfo.appendChild(showImg);
-
-    const likeshow = document.createElement('div');
-    likeshow.id = 'like-show';
-
-    const showTitle = document.createElement('h5');
-    showTitle.id = 'show-title';
-    showTitle.textContent = show.name;
-    likeshow.appendChild(showTitle);
-
-    const likeIcon = document.createElement('i');
-    likeIcon.className = 'fa fa-heart-o';
-    likeIcon.id = 'like-icon';
-    likeIcon.ariaHidden = 'true';
-    likeshow.appendChild(likeIcon);
-
-    showInfo.appendChild(likeshow);
-
-    const allLikes = document.createElement('p');
-    allLikes.id = 'likes';
-    allLikes.textContent = '0 likes';
-    showInfo.appendChild(allLikes);
-
-    const commentButton = document.createElement('button');
-    commentButton.type = 'button';
-    commentButton.id = 'comment-btn';
-    commentButton.textContent = 'Comments';
-    showInfo.appendChild(commentButton);
-
-    allShows.appendChild(showInfo);
-    return show;
-  });
+const displayShows = (show) => {
+  const showInfo = document.createElement('li');
+  showInfo.id = show.id;
+  showInfo.className = 'show';
+  showInfo.innerHTML = `
+  <img id="show-img" src=${show.image} alt="show Image">
+  <div id="like-show">
+    <h2 id="show-title">${show.name}</h2>
+    <i class="fa fa-heart-o" id="like-icon" aria-hidden="true"></i>
+  </div>
+  <p id="likes">${show.likes} likes</p>
+  <button type="button" id="comment-btn">Comments</button>
+  `;
+  allShows.appendChild(showInfo);
 };
 
 export default displayShows;
