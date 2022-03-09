fb95916a1bb07f94324ccfd6d47c694ee755a2d9
levy002 <levy.ukwi002@gmail.com>
2022-03-09 20:56:22 +0200

    Add new like on the show card

--- src/modules/addLike.js
+++ src/modules/addLike.js
@@ -3,10 +3,11 @@ import { postLikes } from './likesCounter.js';
 const addLike = () => {
   document.addEventListener('click', (e) => {
     const clickedElement = e.target;
-    const likesCounter = clickedElement.parentNode.nextElementSibling;
+    const likesCounter = clickedElement.parentNode.nextElementSibling.childNodes[1];
     const showId = Number(clickedElement.parentNode.parentNode.id);
-    if (clickedElement.id === 'like-icon') {
+    if (clickedElement.id === 'liked-icon') {
       postLikes(showId, likesCounter);
+      likesCounter.textContent = (Number(likesCounter.textContent) + 1);
     }
   });
 };
