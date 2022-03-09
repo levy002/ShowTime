353af2ebd7c21dad98507d22c712fb4002cd99f7
levy002 <levy.ukwi002@gmail.com>
2022-03-09 14:08:45 +0200

    Add source files

---
+++ src/modules/displayShow.js
@@ -0,0 +1,27 @@
+import getshow from './getShows.js';
+
+const allShows = document.querySelector('.allShows');
+
+const displayShows = async () => {
+  const fetchedShows = await getshow();
+  const Shows = fetchedShows.slice(0, 20);
+
+  Shows.map((show) => {
+    const showInfo = document.createElement('li');
+    showInfo.id = show.id;
+    showInfo.className = 'show';
+    showInfo.innerHTML = `
+  <img id="show-img" src=${show.image.medium} alt="show Image">
+  <div id="like-show">
+    <h2 id="show-title">${show.name}</h2>
+    <i class="fa fa-heart-o" id="like-icon" aria-hidden="true"></i>
+  </div>
+  <p id="likes">0 likes</p>
+  <button type="button" id="comment-btn">Comments</button>
+  `;
+    allShows.appendChild(showInfo);
+    return show;
+  });
+};
+
+export default displayShows;
