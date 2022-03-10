be5665fa6544e3c3110dd746dab4d19fe0f011b4
levy002 <levy.ukwi002@gmail.com>
2022-03-10 17:56:54 +0200

    Display likes on the page

--- src/modules/addLike.js
+++ src/modules/addLike.js
@@ -3,11 +3,10 @@ import { postLikes } from './likesCounter.js';
 const addLike = () => {
   document.addEventListener('click', (e) => {
     const clickedElement = e.target;
-    const likesCounter = clickedElement.parentNode.nextElementSibling.childNodes[1];
+    const likesCounter = clickedElement.parentNode.nextElementSibling;
     const showId = Number(clickedElement.parentNode.parentNode.id);
     if (clickedElement.id === 'liked-icon') {
       postLikes(showId, likesCounter);
-      likesCounter.textContent = (Number(likesCounter.textContent) + 1);
     }
   });
 };
--- src/modules/displayShow.js
+++ src/modules/displayShow.js
@@ -2,42 +2,42 @@ import getshow from './getShows.js';
 import { getLikesData } from './likesCounter.js';
 
 const allShows = document.querySelector('.allShows');
+allShows.innerHTML = '';
+
+const showTemplate = (Shows) => {
+  Shows.forEach((show) => {
+    const showInfo = document.createElement('li');
+    showInfo.className = 'show';
+    showInfo.id = show.id;
+    showInfo.innerHTML = `
+  <img id="show-img" src=${show.image.medium} alt="show Image">
+  <div id="like-show">
+    <h2 id="show-title">${show.name}</h2>
+    <i class="fas fa-heart" id="liked-icon"></i>
+  </div>
+  <p id="likes">${show.likes}</p><span>likes</span>
+  <button type="button" id="comment-btn">Comments</button>
+  `;
+    allShows.appendChild(showInfo);
+  });
+};
 
 const displayShows = async () => {
   const fetchedShows = await getshow();
   const Shows = fetchedShows.slice(0, 20);
 
   const likesArray = await getLikesData();
-  likesArray.map((a) => {
-    Shows.map((show) => {
+  likesArray.forEach((a) => {
+    Shows.forEach((show) => {
       if (a.item_id === show.id) {
         show.likes = a.likes;
       } else if (show.likes === undefined) {
         show.likes = 0;
       }
-      return show;
     });
-    return a;
   });
 
-  Shows.map((show) => {
-    const showInfo = document.createElement('li');
-    showInfo.id = show.id;
-    showInfo.className = 'show';
-    showInfo.innerHTML = `
-  <img id="show-img" src=${show.image.medium} alt="show Image">
-  <div id="like-show">
-    <h2 id="show-title">${show.name}</h2>
-    <i class="fas fa-heart" id='liked-icon'></i>
-  </div>
-  <div id="likes">
-  <p>${show.likes}</p><span>likes</span>
-  </div>
-  <button type="button" id="comment-btn">Comments</button>
-  `;
-    allShows.appendChild(showInfo);
-    return show;
-  });
+  showTemplate(Shows);
 };
 
 export default displayShows;
