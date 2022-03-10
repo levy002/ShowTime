56c73bd623a5206a93faf55de297a208766df9b2
levy002 <levy.ukwi002@gmail.com>
2022-03-10 22:59:33 +0200

    Display total shows on homapage

--- src/modules/displayShow.js
+++ src/modules/displayShow.js
@@ -3,43 +3,45 @@ import showNumber from './showCounter.js';
 import { getLikesData } from './likesCounter.js';
 
 const allShows = document.querySelector('.allShows');
+const counter = document.getElementById('shows-counter');
+
+const showTemplate = (Shows) => {
+  Shows.forEach((show) => {
+    const showInfo = document.createElement('li');
+    showInfo.className = 'show';
+    showInfo.id = show.id;
+    showInfo.innerHTML = `
+  <img id="show-img" src=${show.image.medium} alt="show Image">
+  <div id="like-show">
+    <h2 id="show-title">${show.name}</h2>
+    <i class="fas fa-heart" id="liked-icon"></i>
+  </div>
+  <div id="likes-counter">
+  <p id="likes">${show.likes}</p>
+  <p>likes</p>
+  </div>
+  <button type="button" id="comment-btn">Comments</button>
+  `;
+    allShows.appendChild(showInfo);
+  });
+};
 
 const displayShows = async () => {
   const fetchedShows = await getshow();
-  const Shows = fetchedShows.slice(200, 240);
-  showNumber(Shows);
-
+  const Shows = fetchedShows.slice(0, 20);
+  counter.textContent = `(${showNumber(Shows)})`;
   const likesArray = await getLikesData();
-  likesArray.map((a) => {
-    Shows.map((show) => {
+  likesArray.forEach((a) => {
+    Shows.forEach((show) => {
       if (a.item_id === show.id) {
         show.likes = a.likes;
       } else if (show.likes === undefined) {
         show.likes = 0;
       }
-      return show;
     });
-    return a;
   });
 
-  Shows.map((show) => {
-    const showInfo = document.createElement('li');
-    showInfo.id = show.id;
-    showInfo.className = 'show';
-    showInfo.innerHTML = `
-  <img id="show-img" src=${show.image.medium} alt="show Image">
-  <div id="like-show">
-    <h2 id="show-title">${show.name}</h2>
-    <i class="fas fa-heart" id='liked-icon'></i>
-  </div>
-  <div id="likes">
-  <p>${show.likes}</p><span>likes</span>
-  </div>
-  <button type="button" id="comment-btn">Comments</button>
-  `;
-    allShows.appendChild(showInfo);
-    return show;
-  });
+  showTemplate(Shows);
 };
 
 export default displayShows;
