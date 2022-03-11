4917575313876576c4c68f9a022875a9dfee3208
Thermiee <navyblack16@yahoo.com>
2022-03-11 20:39:26 +0100

    fix linter errors

--- test/commentCounter.test.js
+++ test/commentCounter.test.js
@@ -1,17 +1,17 @@
-import totalComment from "../src/modules/commentCounter";
+import totalComment from '../src/modules/commentCounter.js';
 
 describe('totalComment', () => {
-    test('result should be equal to 2', () => {
-      const list = [{ name: 'Thermiee' }, { name: 'Jolus' }];
-      const result = totalComment(list);
-  
-      expect(result).toBe(2);
-    });
-  
-    test('result should be equal to 0', () => {
-      const list = [];
-      const result = totalComment(list);
-  
-      expect(result).toBe(0);
-    });
+  test('result should be equal to 2', () => {
+    const list = [{ name: 'Thermiee' }, { name: 'Jolus' }];
+    const result = totalComment(list);
+
+    expect(result).toBe(2);
   });
+
+  test('result should be equal to 0', () => {
+    const list = [];
+    const result = totalComment(list);
+
+    expect(result).toBe(0);
+  });
+});
